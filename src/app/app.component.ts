e4bec2177e5cc7bc0528c74b21d4b06bb200813e
Alanis Kitchen <alaniskitchen47@gmail.com>
2021-02-19 18:31:22 -0700

    Organized structure and designed base footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
   template: `
     <app-navigation></app-navigation>
-    <app-homepage></app-homepage>
+    <router-outlet></router-outlet>>
     <app-footer></app-footer>
     `,
 })
