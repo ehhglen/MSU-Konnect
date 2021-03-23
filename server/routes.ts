63036f470c022afc8e1a95f03b2ea46fb9b9e618
prashant panth <prashantpanth@prashants-MacBook-Pro.local>
2021-03-22 23:43:11 -0600

    post calls

--- server/routes.ts
+++ server/routes.ts
@@ -1,10 +1,33 @@
 import * as express from 'express';
-export const routes = express.Router();
-
-//export { app as routes};
-
+export const app = express.Router();
+var request = require('request');
+export { app as routes};
+var alias: any;
 
 // These are just placeholder routes
-routes.get('/', (req, res) => res.send('Hello World'));
-routes.get('/users', (req, res) => res.send([]));
-routes.post('/users', (req, res) => res.send({body: req.body}));
+
+
+
+
+app.get('/', function(req, res, next) {
+    request({
+      uri: 'https://api.duda.co/api/sites/multiscreen/templates',
+      'headers': {
+        'Authorization': 'Basic ZmJiYjBkOGM6bEZydkNsQzBMTFB2',
+        'Content-Type': 'application/json'
+      }
+    }).pipe(res);
+    
+});
+
+app.post('/template', function(req, res, next) { 
+  request.post({
+    url:'https://api.duda.co/api/sites/multiscreen/create', 
+    headers: 
+    { 
+      authorization: 'Basic ZmJiYjBkOGM6bEZydkNsQzBMTFB2',
+      'content-type': 'application/json' },
+    body:req.body,
+    json: true
+}).pipe(res);
+});
--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
@@ -12,9 +13,17 @@ export class TemplateComponent implements OnInit {
 
     constructor(private http: HttpClient) {}
 
-    post() {
-      this.http.post<any>('http://localhost:4201/template', {username: 'john', password: 'asdf'})
+ 
+
+    onItemClick(template_id:any)
+    {
+
+      
+      this.http.post<any[]>('http://localhost:4201/template' , {template_id:template_id})
       .subscribe(next => console.log(next));
+      //wait for response and then call another post method
+      //how to save the next and pass it to the second function
+
     }
 
   ngOnInit(): void {
